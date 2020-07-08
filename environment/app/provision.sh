009596313c25241d5a042f3f020905f982081d62
Saskia van Barthold <saskia.vanb@gmail.com>
2020-07-08 10:00:34 +0200

    created VPC and bastion

--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -6,20 +6,39 @@ sudo apt-get update -y
 # upgrade any packages available
 sudo apt-get upgrade -y
 
-# install nginx
-sudo apt-get install nginx -y
 
 # install git
 sudo apt-get install git -y
 
+# # disable virtual environment
+# sudo apt-get update
+# sudo apt-get install nginx
+#sudo unlink ~/etc/nginx/sites-enabled/default
+
 # install nodejs
 sudo apt-get install python-software-properties
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install nodejs -y
 
 # install pm2
 sudo npm install pm2 -g
 
+# install nginx
+sudo apt-get install nginx -y
+
+# remove the old file and add our one
+sudo rm /etc/nginx/sites-available/default
+sudo rm /etc/nginx/sites-enabled/default.conf
+
+sudo cp /home/ubuntu/environment/nginx.default /etc/nginx/sites-available/default.conf
+sudo ln -s /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/default.conf
+
+sudo service nginx restart
+
 # npm install and start
 echo 'export DB_HOST=mongodb://192.168.10.200:27017/posts' >> ~/.bashrc
+
+cd /home/ubuntu/app
 sudo npm install
+sudo pm2 start app.js
+sudo systemctl restart nginx
